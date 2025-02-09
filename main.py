77493c8c128d477468a5e8f3a72a523cb0f36c1d
Dheerajll <xettridheeraj01@gmail.com>
2025-02-09 18:07:07 +0545

    Added feature button

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 print("This is a new repo from local machine.")
 print("This is created from local machine.")
-print("This is only in feature 1 and it is a new feature.")  
+print("This is only in feature 1 and it is a new feature. (this is feature1)")  
